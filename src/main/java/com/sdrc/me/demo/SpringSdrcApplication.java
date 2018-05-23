623318a30a6dedde714faf12922c5da33da9bab8
Naseem16 <nas1624@gmail.com>
2018-05-23 15:48:57 +0530

    ThirdCommit

--- src/main/java/com/sdrc/me/demo/SpringSdrcApplication.java
+++ src/main/java/com/sdrc/me/demo/SpringSdrcApplication.java
@@ -8,6 +8,6 @@ public class SpringSdrcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSdrcApplication.class, args);
-		System.out.println("Ramu kaka");
+		System.out.println("abcd");
 	}
 }
