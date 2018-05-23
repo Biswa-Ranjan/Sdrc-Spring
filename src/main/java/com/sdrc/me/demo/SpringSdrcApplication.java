f87c8660562d02a1da1935f28643978dc2c87a2a
Naseem16 <nas1624@gmail.com>
2018-05-23 15:10:04 +0530

    initialCommit

---
+++ src/main/java/com/sdrc/me/demo/SpringSdrcApplication.java
@@ -0,0 +1,12 @@
+package com.sdrc.me.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSdrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSdrcApplication.class, args);
+	}
+}
